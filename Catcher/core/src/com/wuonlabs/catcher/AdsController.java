a42e769475ff289d824245f4f2bc6115afd48853
Daniel <wuonlabs@gmail.com>
2016-07-22 20:43:28 +0000

    libgdx_admob

---
+++ Catcher/core/src/com/wuonlabs/catcher/AdsController.java
@@ -0,0 +1,9 @@
+package com.wuonlabs.catcher;
+/**
+ * Created by Daniel on 2016-07-22.
+ */
+public interface AdsController {
+    public boolean isWifiConnected();
+    public void showBannerAd();
+    public void hideBannerAd();
+}
---
+++ Catcher/desktop/src/com/wuonlabs/catcher/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wuonlabs.catcher.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wuonlabs.catcher.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
